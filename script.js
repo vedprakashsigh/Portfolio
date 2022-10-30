0a99f1fcc1b3ff6c77db7e30cc1faffe7229d2e7
vedprakashsigh <49339994+vedprakashsigh@users.noreply.github.com>
2022-10-30 20:04:32 +0530

    Added Features
    Added Navbar, Hamburger.
    Added More Assets.
    Added Home Page, About Page, Skills Page. Social Media Handles.
    Automatic Dark/Light Mode Added.
    Added Back to Top (changes required)

---
+++ script.js
@@ -0,0 +1,31 @@
+const current = new Date();
+
+const currentTime = current.toLocaleTimeString("en-US", {
+  hour: "2-digit",
+  hour12: false
+});
+
+if (currentTime<"19") { // Light Mode
+    document.querySelector('.nav').style.backgroundColor = "#5F5F5F";
+    document.querySelector('body').style.backgroundColor = "#9C9C9C";
+}
+else { // Dark Mode
+    document.querySelector('.nav').style.backgroundColor = "#9C9C9C";
+    document.querySelector('body').style.backgroundColor = "#5F5F5F";
+}
+
+// Hamburger Operation
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamburger').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle("sidebarGo");
+    if(document.querySelector('.sidebar').classList.contains("sidebarGo")){
+        document.querySelector('.ham').style.display = 'inline'
+        document.querySelector('.cross').style.display = 'none'
+    }
+    else{
+        document.querySelector('.ham').style.display = 'none'
+        setTimeout(() => {
+            document.querySelector('.cross').style.display = 'inline'
+        }, 300);
+    }
+})
