6f4ebc226472a59e13ffef65a1fac05d2540c6fa
vedprakashsigh <49339994+vedprakashsigh@users.noreply.github.com>
2022-10-30 20:26:20 +0530

    Updated back to top

--- script.js
+++ script.js
@@ -2,30 +2,40 @@ const current = new Date();
 
 const currentTime = current.toLocaleTimeString("en-US", {
   hour: "2-digit",
-  hour12: false
+  hour12: false,
 });
 
-if (currentTime<"19") { // Light Mode
-    document.querySelector('.nav').style.backgroundColor = "#5F5F5F";
-    document.querySelector('body').style.backgroundColor = "#9C9C9C";
-}
-else { // Dark Mode
-    document.querySelector('.nav').style.backgroundColor = "#9C9C9C";
-    document.querySelector('body').style.backgroundColor = "#5F5F5F";
+if (currentTime < "19") {
+  // Light Mode
+  document.querySelector(".nav").style.backgroundColor = "#5F5F5F";
+  document.querySelector("body").style.backgroundColor = "#9C9C9C";
+} else {
+  // Dark Mode
+  document.querySelector(".nav").style.backgroundColor = "#9C9C9C";
+  document.querySelector("body").style.backgroundColor = "#5F5F5F";
 }
 
 // Hamburger Operation
-document.querySelector('.cross').style.display='none';
-document.querySelector('.hamburger').addEventListener("click",()=>{
-    document.querySelector('.sidebar').classList.toggle("sidebarGo");
-    if(document.querySelector('.sidebar').classList.contains("sidebarGo")){
-        document.querySelector('.ham').style.display = 'inline'
-        document.querySelector('.cross').style.display = 'none'
-    }
-    else{
-        document.querySelector('.ham').style.display = 'none'
-        setTimeout(() => {
-            document.querySelector('.cross').style.display = 'inline'
-        }, 300);
-    }
-})
+document.querySelector(".cross").style.display = "none";
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".sidebar").classList.toggle("sidebarGo");
+  if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
+    document.querySelector(".ham").style.display = "inline";
+    document.querySelector(".cross").style.display = "none";
+  } else {
+    document.querySelector(".ham").style.display = "none";
+    setTimeout(() => {
+      document.querySelector(".cross").style.display = "inline";
+    }, 300);
+  }
+});
+
+// Back to Top disapper when at top
+window.addEventListener("scroll", function (event) {
+  var scroll_y = this.scrollY;
+  if (scroll_y < 300) {
+    document.querySelector("#position").style.display = "none";
+  } else {
+    document.querySelector("#position").style.display = "inline";
+  }
+});
